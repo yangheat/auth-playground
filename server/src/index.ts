acee4fdda944e969d9b8f37996b58a8167e2fc68
yangheat <yahgneat@gmail.com>
2026-04-26 23:42:29 +0900

    평문 텍스트를 통해 로그인 API 동작하도록 기능 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,36 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const testUser = {
+  username: "test",
+  password: "1234",
+};
+
+const app = new Elysia()
+  .group("/api", (app) =>
+    app
+      .get("/", () => "Hello Elysia")
+      .post(
+        "/auth/login",
+        ({ body, status }) => {
+          const { username, password } = body;
+
+          if (
+            username !== testUser.username ||
+            password !== testUser.password
+          ) {
+            return status(401, "Invalid username or password");
+          }
+          return status(200, "Login successful");
+        },
+        {
+          body: t.Object({
+            username: t.String(),
+            password: t.String(),
+          }),
+        },
+      ),
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
