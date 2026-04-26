2b84d828d357e7ffc96b5563007d8ce640a5eb19
yangheat <yahgneat@gmail.com>
2026-04-26 22:48:05 +0900

    CORS 정책 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // 로컬 환경 테스트에서 CORS 정책 문제 해결을 위해 프록시 설정
+      // api 요청은 3000번 포트로 전달
+      "/api": { target: "http://localhost:3000", changeOrigin: true },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
