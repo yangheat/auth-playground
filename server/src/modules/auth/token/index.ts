edc03d09722b3df3e5d0ad1c01653463e9330442
yangheat <yahgneat@gmail.com>
2026-05-04 20:35:13 +0900

    refresh token 기능 추가

--- server/src/modules/auth/token/index.ts
+++ server/src/modules/auth/token/index.ts
@@ -3,6 +3,10 @@ import Elysia from "elysia";
 import { Auth } from "../service";
 import { AuthModel } from "../model";
 
+const refreshTokens = new Map<string, { iat: number; exp: number }>();
+
+function createAccessToken() {}
+
 export const token = new Elysia({ prefix: "token" })
   .use(
     jwt({
@@ -12,23 +16,46 @@ export const token = new Elysia({ prefix: "token" })
     }),
   )
   .get("/", async ({ headers, status, jwt }) => {
-		const accessToken = headers.authorization?.split(' ')
-		const profile = await jwt.verify(accessToken?.[1])
+    const accessToken = headers.authorization?.split(" ")[1];
+    const profile = await jwt.verify(accessToken);
 
-		if (!profile) {
-			return status(401, "Unauthorized");
-		}
+    if (profile) {
+      return status(200);
+    }
 
-		return status(200)
-	})
+    const cookies = new URLSearchParams(headers.cookie);
+    const refreshToken = cookies.get("refreshToken");
+
+    if (refreshToken) {
+      const storedRefreshToken = refreshTokens.get(refreshToken);
+      const now = Math.floor(Date.now());
+
+      if (storedRefreshToken && now > storedRefreshToken.exp) {
+        const accessToken = await jwt.sign({});
+        return status(200, { accessToken });
+      }
+    }
+
+    return status(401, "Unauthorized");
+  })
   .post(
     "/login",
-    async ({ body, jwt, status }) => {
+    async ({ body, cookie, jwt, status }) => {
       const result = await Auth.verifyCredentials(body);
+
       if (!result) {
         return status(401, "Invalid username or password");
       }
-      const accessToken = await jwt.sign({ sub: body.username });
+      
+      const refreshToken = crypto.randomUUID();
+      const timestamp = Math.floor(Date.now() / 1000);
+      refreshTokens.set(refreshToken, {
+        iat: timestamp,
+        exp: timestamp + 2,
+      });
+      cookie.refreshToken.set({ value: refreshToken });
+
+      const accessToken = await jwt.sign({});
       return status(200, { accessToken });
     },
     {
